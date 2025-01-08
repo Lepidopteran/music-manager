455f4084eb9115e0fbff61561fb07106f0a8f263
Blaine <blainenicholls@proton.me>
2025-01-08 17:27:23 -0500

    refactor(albums api): get_albums return all information

--- src/app/api/albums.rs
+++ src/app/api/albums.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 use axum::{
     extract::{Path, State},
     http::StatusCode,
@@ -53,18 +55,39 @@ struct AlbumMetadata {
 
 async fn get_albums(
     State(db): State<sqlx::Pool<sqlx::Sqlite>>,
-) -> Result<Json<Vec<AlbumMetadata>>, impl IntoResponse> {
-    match query_as!(
-        AlbumMetadata,
-        "SELECT album AS title, album_artist AS artist FROM songs WHERE album IS NOT NULL GROUP by album"
-    )
-    .fetch_all(&db)
-    .await
+) -> Result<Json<Vec<Album>>, impl IntoResponse> {
+    let tracks = match query_as!(Song, "SELECT * FROM songs WHERE album IS NOT NULL")
+        .fetch_all(&db)
+        .await
     {
-        Ok(albums) => Ok(Json(albums)),
+        Ok(tracks) => tracks,
         Err(err) => {
             tracing::error!("{}", err);
-            Err(internal_error(err))
+            return Err(internal_error(err));
         }
+    };
+
+    if tracks.is_empty() {
+        return Err((StatusCode::NOT_FOUND, "No albums found".to_string()));
     }
+
+    let mut album_map: HashMap<String, Vec<Song>> = HashMap::new();
+
+    for track in tracks {
+        album_map
+            .entry(track.album.clone().expect("Album not found"))
+            .or_default()
+            .push(track);
+    }
+
+    let albums: Vec<Album> = album_map
+        .into_iter()
+        .filter_map(|(_, tracks)| Album::try_from(tracks).ok())
+        .collect();
+
+    if albums.is_empty() {
+        return Err((StatusCode::INTERNAL_SERVER_ERROR, "Failed to construct albums".to_string()));
+    }
+
+    Ok(Json(albums))
 }
--- src/metadata/album.rs
+++ src/metadata/album.rs
@@ -1,7 +1,7 @@
-use super::{get_cover_art, CoverArt, Song};
-use std::{fs::read_dir, path::Path};
+use super::Song;
 
 #[derive(Debug, Clone, Default, serde::Serialize)]
+#[serde(rename_all = "camelCase")]
 pub struct Album {
     pub title: String,
     pub tracks: Vec<Song>,
@@ -12,59 +12,7 @@ pub struct Album {
     pub artist: Option<String>,
     pub label: Option<String>,
     pub date: Option<time::Date>,
-}
-
-impl Album {
-    // pub fn cover_art(&self) -> Option<CoverArt> {
-    //     let path = self
-    //         .tracks
-    //         .first()
-    //         .map(|song| &song.path.clone())
-    //         .and_then(|path| Path::new(path).parent().map(|path| path.to_path_buf()));
-    //
-    //     let path = path?;
-    //
-    //     let mut cover_art: Option<CoverArt> = None;
-    //
-    //     for entry in read_dir(&path).ok()? {
-    //         let entry = entry.ok()?;
-    //
-    //         if !entry.file_type().ok()?.is_file() {
-    //             continue;
-    //         }
-    //
-    //         let stem = entry
-    //             .path()
-    //             .file_stem()?
-    //             .to_string_lossy()
-    //             .to_string()
-    //             .to_lowercase();
-    //
-    //         let file_type = entry
-    //             .path()
-    //             .extension()?
-    //             .to_string_lossy()
-    //             .to_string()
-    //             .to_lowercase();
-    //
-    //         if !["jpg", "jpeg", "png", "gif", "bmp", "tiff", "tif", "webp"]
-    //             .iter()
-    //             .any(|name| file_type.ends_with(name))
-    //         {
-    //             continue;
-    //         }
-    //
-    //         if !["cover", "folder", "albumart"]
-    //             .iter()
-    //             .any(|name| stem.starts_with(name))
-    //         {
-    //             continue;
-    //         }
-    //
-    //
-    //
-    //     }
-    // }
+    pub original_date: Option<time::Date>,
 }
 
 impl TryFrom<Vec<Song>> for Album {
@@ -80,7 +28,6 @@ impl TryFrom<Vec<Song>> for Album {
         if rest.is_empty() {
             return Ok(Self {
                 title,
-                tracks: tracks.to_vec(),
                 artist: first.album_artist.clone(),
                 ..Default::default()
             });
@@ -92,7 +39,7 @@ impl TryFrom<Vec<Song>> for Album {
 
         Ok(Self {
             title,
-            tracks: tracks.to_vec(),
+            tracks: tracks.clone(),
             artist: first.album_artist.clone(),
             ..Default::default()
         })
