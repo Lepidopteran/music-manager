8de71e69a28248242ad0329f227b09432f7d65cd
Blaine <blainenicholls@proton.me>
2025-01-08 23:36:48 -0500

    feat(metadata): sanatize and parse tags properly

--- src/metadata/mod.rs
+++ src/metadata/mod.rs
@@ -1,13 +1,8 @@
-use id3::Tag;
-
 mod album;
 mod cover_art;
-mod song;
 mod error;
+mod song;
+
+pub mod tags;
 
 pub use {album::*, cover_art::*, song::*};
-
-pub fn get_tag(path: &str) -> Option<Tag> {
-    let tag = Tag::read_from_path(path).ok()?;
-    Some(tag)
-}
--- src/metadata/song.rs
+++ src/metadata/song.rs
@@ -7,6 +7,8 @@ use mp4ameta::Tag as Mp4Tag;
 use serde::{Deserialize, Serialize};
 use sqlx::prelude::*;
 
+use super::tags::{parse_tags, sanitize_tag, ParserConfig};
+
 #[derive(Deserialize, Serialize, FromRow, Debug, Clone, Default)]
 #[serde(rename_all = "camelCase")]
 pub struct Song {
@@ -45,21 +47,28 @@ impl Song {
             None => return Err("No file type found".to_string()),
         };
 
+        // TODO: Remove default parser config for customization
+        let config = ParserConfig::new();
+
         match file_type.as_str() {
             "mp3" | "wav" | "aif" | "aiff" => {
                 let tag = Id3Tag::read_from2(file).map_err(|err| err.to_string())?;
 
                 Ok(Self {
                     title: match tag.title() {
-                        Some(title) => Some(title.to_string()),
+                        Some(title) => Some(sanitize_tag(title)),
                         None => stem,
                     },
-                    artist: tag.artist().map(|artist| artist.to_string()),
-                    album: tag.album().map(|album| album.to_string()),
+                    artist: tag
+                        .artist()
+                        .map(|artist| parse_tags(artist, &config).join(", ")),
+                    album: tag.album().map(sanitize_tag),
                     album_artist: tag
                         .album_artist()
-                        .map(|album_artist| album_artist.to_string()),
-                    genre: tag.genre().map(|genre| genre.to_string()),
+                        .map(|album_artist| parse_tags(album_artist, &config).join(", ")),
+                    genre: tag
+                        .genre()
+                        .map(|genre| parse_tags(genre, &config).join(", ")),
                     track_number: tag.track().map(|track| track.to_string()),
                     disc_number: tag.disc().map(|disc| disc.to_string()),
                     year: tag.year().map(|year| year.to_string()),
@@ -84,10 +93,14 @@ impl Song {
                     album: comments.album().map(|album| album[0].to_string()),
                     album_artist: comments
                         .album_artist()
-                        .map(|album_artist| album_artist[0].to_string()),
-                    genre: comments.genre().map(|genre| genre[0].to_string()),
+                        .map(|album_artist| parse_tags(&album_artist[0], &config).join(", ")),
+                    genre: comments
+                        .genre()
+                        .map(|genre| parse_tags(&genre[0], &config).join(", ")),
                     track_number: comments.track().map(|track| track.to_string()),
-                    disc_number: comments.get("DISCNUMBER").map(|disc| disc[0].to_string()),
+                    disc_number: comments
+                        .get("DISCNUMBER")
+                        .map(|disc| sanitize_tag(&disc[0])),
                     ..Default::default()
                 })
             }
@@ -96,12 +109,16 @@ impl Song {
 
                 Ok(Self {
                     title: stem,
-                    artist: tag.artist().map(|artist| artist.to_string()),
-                    album: tag.album().map(|album| album.to_string()),
+                    artist: tag
+                        .artist()
+                        .map(|artist| parse_tags(artist, &config).join(", ")),
+                    album: tag.album().map(sanitize_tag),
                     album_artist: tag
                         .album_artist()
-                        .map(|album_artist| album_artist.to_string()),
-                    genre: tag.genre().map(|genre| genre.to_string()),
+                        .map(|album_artist| parse_tags(album_artist, &config).join(", ")),
+                    genre: tag
+                        .genre()
+                        .map(|genre| parse_tags(genre, &config).join(", ")),
                     track_number: tag.track_number().map(|track| track.to_string()),
                     disc_number: tag.disc_number().map(|disc| disc.to_string()),
                     ..Default::default()
