f02f1a99639afa4f5ae65324cc954283d1bff2d9
Blaine <blainenicholls@proton.me>
2025-01-22 12:23:19 -0500

    refactor(metadata): migrate cover_art to use lofty

--- src/metadata/cover_art.rs
+++ src/metadata/cover_art.rs
@@ -1,6 +1,9 @@
 use std::{fmt::Debug, path::Path};
 
 use id3::{frame::PictureType as Id3PictureType, Tag as Id3Tag};
+use lofty::probe::Probe;
+use lofty::tag::TagType;
+use lofty::{picture::PictureType, prelude::*};
 use metaflac::{block::PictureType as FlacPictureType, Tag as FlacTag};
 use mp4ameta::{ImgFmt, Tag as Mp4Tag};
 
@@ -21,6 +24,16 @@ impl TryFrom<&str> for CoverArtType {
             "back" => Ok(CoverArtType::Back),
             "other" => Ok(CoverArtType::Other),
             _ => Err(format!("Invalid cover art type: {}", value))?,
+        }
+    }
+}
+
+impl From<PictureType> for CoverArtType {
+    fn from(value: PictureType) -> Self {
+        match value {
+            PictureType::CoverFront => CoverArtType::Front,
+            PictureType::CoverBack => CoverArtType::Back,
+            _ => CoverArtType::Other,
         }
     }
 }
@@ -62,7 +75,6 @@ impl Debug for CoverArt {
     }
 }
 
-// TODO: Add opus cover art
 /// Get all the cover art from a file
 ///
 /// Returns an empty vector if no cover art is found
@@ -73,66 +85,58 @@ pub fn get_cover_art(path: &str) -> Vec<CoverArt> {
         return Vec::new();
     }
 
-    let file_type = match path.extension().and_then(|ext| ext.to_str()) {
-        Some(ext) => ext,
-        None => return Vec::new(),
+    let tagged_file = match Probe::open(path) {
+        Ok(tag) => tag.read().unwrap(),
+        Err(_) => return Vec::new(),
     };
 
-    match file_type {
-        "mp3" | "wav" | "aif" | "aiff" => {
-            let tags = match Id3Tag::read_from_path(path) {
-                Ok(tag) => tag,
-                Err(_) => return Vec::new(),
-            };
+    let tag = tagged_file.primary_tag().unwrap();
 
-            tags.pictures()
-                .filter(|picture| picture.mime_type.starts_with("image/"))
-                .map(|picture| CoverArt {
-                    cover_type: picture.picture_type.into(),
-                    mime_type: picture.mime_type.clone(),
-                    data: picture.data.clone(),
-                })
-                .collect()
-        }
-        "flac" => {
-            let tags = match FlacTag::read_from_path(path) {
-                Ok(tag) => tag,
-                Err(_) => return Vec::new(),
-            };
+    if tag.is_empty() {
+        return Vec::new();
+    }
 
-            tags.pictures()
-                .filter(|picture| picture.mime_type.starts_with("image/"))
-                .map(|picture| CoverArt {
-                    cover_type: picture.picture_type.into(),
-                    mime_type: picture.mime_type.to_string(),
-                    data: picture.data.to_vec(),
-                })
-                .collect()
-        }
-        "mp4" | "m4a" => {
-            let tags = match Mp4Tag::read_from_path(path) {
-                Ok(tag) => tag,
-                Err(_) => return Vec::new(),
-            };
+    let pictures = tag.pictures().iter();
 
-            tags.artworks()
-                .enumerate()
-                .map(|(index, artwork)| CoverArt {
+    if pictures.len() == 0 {
+        return Vec::new();
+    }
 
-                    // NOTE: I'm assuming the cover art is always the first artwork in the list.
-                    cover_type: match index {
-                        0 => CoverArtType::Front,
-                        1 => CoverArtType::Back,
-                        _ => CoverArtType::Other,
-                    },
-                    mime_type: match artwork.fmt {
-                        ImgFmt::Jpeg => "image/jpeg".to_string(),
-                        ImgFmt::Png => "image/png".to_string(),
-                        ImgFmt::Bmp => "image/bmp".to_string(),
-                    },
-                    data: artwork.data.to_vec(),
-                })
-                .collect()
+    if tag.tag_type() == TagType::Mp4Ilst {
+        pictures
+            .enumerate()
+            .map(|(index, picture)| CoverArt {
+                // NOTE: The first picture should be the front cover in most cases
+                cover_type: if index == 0 {
+                    CoverArtType::Front
+                } else {
+                    CoverArtType::Other
+                },
+                mime_type: picture.mime_type().unwrap().as_str().to_string(),
+                data: picture.data().to_vec(),
+            })
+            .collect()
+    } else if pictures.len() > 1 {
+        pictures
+            .map(|picture| CoverArt {
+                cover_type: picture.pic_type().into(),
+                mime_type: picture.mime_type().unwrap().as_str().to_string(),
+                data: picture.data().to_vec(),
+            })
+            .collect()
+    } else {
+        pictures
+            .filter(|picture| {
+                [PictureType::CoverFront, PictureType::Other].contains(&picture.pic_type())
+            })
+            .map(|picture| CoverArt {
+                cover_type: PictureType::CoverFront.into(),
+                mime_type: picture.mime_type().unwrap().as_str().to_string(),
+                data: picture.data().to_vec(),
+            })
+            .collect()
+    }
+}
         }
         _ => Vec::new(),
     }
