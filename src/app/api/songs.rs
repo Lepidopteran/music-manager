585d167147322bb3780d484740cf61a2cd367715
Blaine <blainenicholls@proton.me>
2025-01-26 13:16:08 -0500

    feat(api): add ability to refresh specific song metadata

--- src/app/api/songs.rs
+++ src/app/api/songs.rs
@@ -1,14 +1,19 @@
+use std::path::PathBuf;
+
 use axum::{
     extract::{Path, State},
+    http::StatusCode,
     response::IntoResponse,
-    routing::get,
+    routing::{get, post},
     Json, Router,
 };
-use sqlx::query_as;
+use sqlx::{query_as, query_scalar};
+use tokio::task::spawn_blocking;
 
 use crate::{
     app::AppState,
     db::Song,
+    metadata::{SongFile, SongMetadata},
     utils::*,
 };
 
@@ -16,6 +21,7 @@ pub fn router() -> Router<AppState> {
     Router::new()
         .route("/api/songs/", get(get_songs))
         .route("/api/songs/:id", get(get_song))
+        .route("/api/songs/:id/refresh", post(refresh_song_details))
 }
 
 async fn get_song(
@@ -28,6 +34,41 @@ async fn get_song(
         .await
         .map(Json)
         .map_err(internal_error)
+}
+
+async fn refresh_song_details(
+    State(db): State<sqlx::Pool<sqlx::Sqlite>>,
+    Path(song_id): Path<i32>,
+) -> Result<Json<SongMetadata>, (StatusCode, String)> {
+    let path = query_scalar!("SELECT path FROM songs WHERE id = ?", song_id)
+        .fetch_one(&db)
+        .await
+        .map(PathBuf::from)
+        .map_err(internal_error)?;
+
+    let file = spawn_blocking(move || {
+        SongFile::open(&path).map_err(|err| (StatusCode::INTERNAL_SERVER_ERROR, err))
+    })
+    .await
+    .map_err(internal_error)??;
+
+    let metadata = file.metadata().clone();
+
+    sqlx::query("UPDATE songs SET title = ?, artist = ?, album = ?, album_artist = ?, genre = ?, track_number = ?, disc_number = ?, year = ? WHERE id = ?")
+        .bind(metadata.title.clone())
+        .bind(metadata.artist.clone())
+        .bind(metadata.album.clone())
+        .bind(metadata.album_artist.clone())
+        .bind(metadata.genre.clone())
+        .bind(metadata.track_number.clone())
+        .bind(metadata.disc_number.clone())
+        .bind(metadata.year.clone())
+        .bind(song_id)
+        .execute(&db)
+        .await
+        .map_err(internal_error)?;
+
+    Ok(Json(metadata))
 }
 
 async fn get_songs(
