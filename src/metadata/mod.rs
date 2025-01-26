3da24d983c15c036b2b2cee853cc65aa575cb1b3
Blaine <blainenicholls@proton.me>
2025-01-26 13:12:43 -0500

    feat(metadata): create SongFile for file info of songs

--- src/metadata/mod.rs
+++ src/metadata/mod.rs
@@ -2,7 +2,7 @@ mod album;
 mod cover_art;
 mod error;
 mod song;
+mod file;
 
 pub mod tags;
-
 pub use {album::*, cover_art::*, song::*};
--- src/metadata/song.rs
+++ src/metadata/song.rs
@@ -1,11 +1,85 @@
-use std::path::Path;
+use std::{
+    path::{Path, PathBuf},
+    time::SystemTime,
+};
 
-use lofty::prelude::*;
 use lofty::probe::Probe;
+use lofty::{prelude::*, tag::Tag};
 use serde::{Deserialize, Serialize};
-use sqlx::prelude::*;
 
-use super::tags::sanitize_tag;
+use super::{
+    file::SongFileType,
+    tags::{sanitize_tag, TagType},
+};
+
+#[derive(Deserialize, Serialize, Debug, Clone)]
+#[serde(rename_all = "camelCase")]
+pub struct SongFile {
+    path: PathBuf,
+    tag_type: Option<TagType>,
+    file_type: SongFileType,
+    metadata: SongMetadata,
+    created: SystemTime,
+    last_modified: SystemTime,
+    size: u64,
+}
+
+impl SongFile {
+    pub fn open(path: &Path) -> Result<Self, String> {
+        check_path(path)?;
+
+        let path_metadata = path.metadata().map_err(|err| err.to_string())?;
+
+        let tagged_file = match Probe::open(path) {
+            Ok(tag) => tag.read().map_err(|err| err.to_string())?,
+            Err(err) => return Err(err.to_string()),
+        };
+
+        let tag = match tagged_file.primary_tag() {
+            Some(tag) => tag,
+            None => return Err("No tag found".to_string()),
+        };
+
+        Ok(Self {
+            path: path.to_path_buf(),
+            size: path_metadata.len(),
+            tag_type: Some(tag.tag_type().into()),
+            file_type: SongFileType::from(tagged_file.file_type()),
+            last_modified: path_metadata.modified().map_err(|err| err.to_string())?,
+            created: path_metadata.created().map_err(|err| err.to_string())?,
+            metadata: SongMetadata::from(tag),
+        })
+    }
+
+    pub fn path(&self) -> &PathBuf {
+        &self.path
+    }
+
+    pub fn metadata(&self) -> &SongMetadata {
+        &self.metadata
+    }
+
+    pub fn metadata_mut(&mut self) -> &mut SongMetadata {
+        &mut self.metadata
+    }
+
+    pub fn read(&mut self) -> Result<(), String> {
+        self.metadata = SongMetadata::from_path(&self.path).map_err(|err| err.to_string())?;
+        Ok(())
+    }
+
+    pub fn size(&self) -> u64 {
+        self.size
+    }
+
+    pub fn last_modified(&self) -> SystemTime {
+        self.last_modified
+    }
+
+    pub fn created(&self) -> SystemTime {
+        self.created
+    }
+}
 
 #[derive(Deserialize, Serialize, Debug, Clone, Default)]
 #[serde(rename_all = "camelCase")]
@@ -69,4 +143,10 @@ pub fn check_path(path: &Path) -> Result<(), String> {
     }
 
     Ok(())
+}
+
+impl From<SongFile> for SongMetadata {
+    fn from(file: SongFile) -> Self {
+        file.metadata
+    }
 }
