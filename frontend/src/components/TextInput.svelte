9392982a43b2fb4598b3d7e13d0dc9b0f98fc02c
Blaine <blainenicholls@proton.me>
2025-08-15 13:09:59 -0400

    refactor(frontend): don't show label if no label is specified in TextInput

--- frontend/src/components/TextInput.svelte
+++ frontend/src/components/TextInput.svelte
@@ -19,16 +19,18 @@
 </script>
 
 <div class="flex flex-col">
-	<div class="flex justify-between">
-		<label class="label" for="text-input">
-			<span class="text-base-950">
-				{label}:
-				{#if required}
-					<span class="text-red-500 font-bold select-none">*</span>
-				{/if}
-			</span>
-		</label>
-	</div>
+	{#if label}
+		<div class="flex justify-between">
+			<label class="label" for="text-input">
+				<span class="text-base-950">
+					{label}:
+					{#if required}
+						<span class="text-red-500 font-bold select-none">*</span>
+					{/if}
+				</span>
+			</label>
+		</div>
+	{/if}
 	<input
 		class="bg-base-300 px-2 py-1 inset-shadow-sm inset-shadow-black/25 rounded-theme focus-visible:outline-1 focus-visible:outline-primary"
 		type="text"
