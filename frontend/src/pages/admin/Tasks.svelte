a3cf22352673adf62db79b6f42baf7cf2dae9ae2
Blaine <blainenicholls@proton.me>
2025-08-21 13:55:07 -0400

    feat(frontend): get basic functionality to tasks page

--- frontend/src/pages/admin/Tasks.svelte
+++ frontend/src/pages/admin/Tasks.svelte
@@ -1,12 +1,12 @@
 <script lang="ts">
-	import { getTasks } from "@api/tasks";
+	import { getTasks, startTask, stopTask } from "@api/tasks";
 	import Event from "@components/Event.svelte";
 	import Progress from "@components/Progress.svelte";
 	import type { TaskEvent, TaskInfo } from "@lib/models";
 	import { taskEventsSource } from "@lib/state/event-sources.svelte";
 	import { addSourceEventListener } from "@lib/utils/api";
 	import { prefersReducedMotion } from "svelte/motion";
-	import { slide } from "svelte/transition";
+	import { fade, slide } from "svelte/transition";
 	import { onMount } from "svelte";
 	import Button from "@components/Button.svelte";
 	import Icon from "@iconify/svelte";
@@ -16,6 +16,24 @@
 	const events: Array<TaskEvent> = $state([]);
 	addSourceEventListener(taskEventsSource, "task-event", (event) => {
 		events.push(event);
+
+		tasks = tasks.map((task) => {
+			if (task.id !== event.source) {
+				return task;
+			}
+
+			switch (event.kind) {
+				case "stop":
+					return { ...task, status: "stopped" };
+				case "progress":
+				case "start":
+					return { ...task, status: "running" };
+				case "complete":
+					return { ...task, status: "idle" };
+			}
+
+			return task;
+		});
 	});
 
 	$effect(() => {
@@ -36,38 +54,62 @@
 			<li>
 				<div
 					data-id={task.id}
-					class="bg-base-100 max-w-4xl mx-auto overflow-hidden rounded-theme shadow-md"
+					class={`bg-base-100 max-w-4xl mx-auto overflow-hidden rounded-theme shadow-md`}
 				>
 					<div class="flex divide-x-2 divide-base-text/25 w-full">
 						{#each { length: task.steps }, step}
 							{@const lastProgress = logs.findLast(
-								(e) => e.kind === "progress",
+								(e) =>
+									(e.kind === "progress" && e.step === step + 1) ||
+									(e.kind === "progress" && !e.step),
 							)}
 							<Progress
-								value={lastProgress?.current ?? 0}
+								value={task.status === "running"
+									? (lastProgress?.current ?? 0)
+									: 0}
 								max={lastProgress?.total ?? 100}
 								class="rounded-none! w-full"
 							/>
 						{/each}
 					</div>
-					<p class="font-bold text-lg pt-2 pl-2">{task.name}</p>
-					<p class="pl-2">{task.description}</p>
+					<div class="flex justify-between items-center pr-4">
+						<div>
+							<p class="font-bold text-lg pt-2 pl-2">{task.name}</p>
+							<p class="pl-2">{task.description}</p>
+						</div>
+						<div>
+							<Button
+								aria-label={task.status === "running"
+									? `Stop ${task.name}`
+									: `Start ${task.name}`}
+								variant="primary"
+								onclick={async () => {
+									if (task.status !== "running") {
+										startTask(task.id);
+									} else {
+										stopTask(task.id);
+									}
+								}}
+							>
+								{#if task.status === "running"}
+									<Icon icon="mingcute:stop-fill" />
+								{:else}
+									<Icon icon="mingcute:play-fill" />
+								{/if}
+							</Button>
+						</div>
+					</div>
 					<details class="space-y-2 mt-2 px-2">
 						<summary
-							class="flex items-center gap-2 text-sm font-bold text-base-950/50 overflow-hidden"
+							class="flex items-center gap-2 text-sm font-bold text-base-950/50 cursor-pointer select-none overflow-hidden"
 						>
-							Event Logs <span
-								transition:slide={{
-									duration: prefersReducedMotion ? 0 : 200,
-									axis: "y",
-								}}
-								class="text-base-text/25 truncate w-32"
+							Event Logs <span class="text-base-text/25 truncate w-32"
 								>{logs.at(-1)?.message}</span
 							>
 						</summary>
 						<div class="space-y-2 mt-2 p-2 overflow-y-auto h-48">
 							{#each logs as event, index}
-								<Event data-log-index={index} {event} class="truncate" />
+								<Event data-log-index={index} {event} />
 							{/each}
 						</div>
 					</details>
