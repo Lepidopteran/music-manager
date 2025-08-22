b065f8bb7ee5285156a646c9c5dacba688d9d425
Blaine <blainenicholls@proton.me>
2025-08-22 16:06:32 -0400

    chore(frontend): remove unused declare and function in tasks api

--- frontend/src/lib/api/tasks.ts
+++ frontend/src/lib/api/tasks.ts
@@ -1,12 +1,6 @@
 import type { TaskEvent, TaskInfo } from "@lib/models";
 import { fetchJson, fetchText } from "@lib/utils/api";
 
-declare global {
-	interface EventSourceEventMap {
-		"task-event": MessageEvent<TaskEvent>;
-	}
-}
-
 export async function getTasks(): Promise<TaskInfo[]> {
 	return await fetchJson<TaskInfo[]>("/api/tasks");
 }
@@ -17,8 +11,4 @@ export async function startTask(id: string): Promise<void> {
 
 export async function stopTask(id: string): Promise<void> {
 	await fetchText(`/api/tasks/${id}/stop`);
-}
-
-export async function getEventStream(): Promise<EventSource> {
-	return new EventSource("/api/tasks/events");
 }
