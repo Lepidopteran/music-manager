8db15e0d1b6513008163f34afb83eeab9c51529d
Blaine <blainenicholls@proton.me>
2025-08-25 15:10:35 -0400

    feat(components): add various properties to text input

--- frontend/src/components/TextInput.svelte
+++ frontend/src/components/TextInput.svelte
@@ -1,44 +1,100 @@
 <script lang="ts">
-	interface Props {
+	import type { HTMLInputAttributes } from "svelte/elements";
+	interface Props extends HTMLInputAttributes {
 		label?: string;
 		value?: string;
-		placeholder?: string;
-		pattern?: string;
-		required?: boolean;
+		variant?: "base" | "ghost";
+		floatingLabel?: boolean;
 		[rest: string]: unknown;
 	}
 
 	let {
 		label,
 		value = $bindable(""),
+		variant = "base",
+		floatingLabel = false,
+		required = true,
 		placeholder,
 		pattern,
-		required = false,
+		class: className,
 		...rest
 	}: Props = $props();
 </script>
 
-<div class="flex flex-col">
+<label class={`${floatingLabel ? "floating-label" : ""} block`}>
 	{#if label}
-		<div class="flex justify-between">
-			<label class="label" for="text-input">
-				<span class="text-base-950">
-					{label}:
-					{#if required}
-						<span class="text-red-500 font-bold select-none">*</span>
-					{/if}
-				</span>
-			</label>
-		</div>
+		<span class="text-base-950 block">
+			{label}
+		</span>
 	{/if}
 	<input
-		class="bg-base-300 px-2 py-1 inset-shadow-sm inset-shadow-black/25 rounded-theme focus-visible:outline-1 focus-visible:outline-primary"
+		class={`inset-shadow-sm inset-shadow-black/25 focus-visible:outline-1 focus-visible:outline-primary ${`input-${variant}`} ${className || ""}`}
 		type="text"
-		id="text-input"
-		{required}
-		{pattern}
-		{placeholder}
+		placeholder={placeholder || label}
 		bind:value
 		{...rest}
 	/>
-</div>
+</label>
+
+<style>
+	input {
+		padding: calc(var(--spacing) * 2) calc(var(--spacing));
+		border-radius: var(--radius-theme);
+
+		&:disabled,
+		&[disabled] {
+			cursor: not-allowed;
+			opacity: 0.5;
+		}
+
+		&.input-base {
+			background-color: var(--color-base-300);
+		}
+
+		&.input-ghost {
+			background-color: transparent;
+			box-shadow: none;
+		}
+	}
+
+	.floating-label {
+		position: relative;
+		display: block;
+
+		& > input {
+			padding: calc(var(--spacing) * 4) calc(var(--spacing) * 2);
+			display: block;
+		}
+
+		& > input::placeholder {
+			transition: all 200ms ease-in-out;
+		}
+
+		& > span {
+			position: absolute;
+			pointer-events: none;
+			user-select: none;
+			top: 50%;
+			transform: translateY(-50%);
+			left: 0.5rem;
+			opacity: 0;
+			transition: all 200ms ease-in-out;
+		}
+
+		&:focus-within,
+		&:not(:has(:placeholder-shown)) {
+			& > input::placeholder {
+				opacity: 0;
+			}
+
+			& > span {
+				top: 0;
+				transform: translateY(10%);
+				font-size: var(--text-xs);
+				line-height: var(--text-xs--line-height);
+				font-weight: var(--font-weight-bold);
+				opacity: 0.5;
+			}
+		}
+	}
+</style>
