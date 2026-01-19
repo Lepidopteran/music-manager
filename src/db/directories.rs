18a3879fe9e98d6411a1708c5d85cb69f44006d7
Blaine <blainenicholls@protonmail.com>
2026-01-19 12:20:44 -0500

    feat: add single db directory get function

--- src/db/directories.rs
+++ src/db/directories.rs
@@ -151,3 +151,14 @@ pub async fn get_directories<'c>(
 
     Ok(directories)
 }
+
+pub async fn get_directory<'c>(
+    connection: impl sqlx::Acquire<'c, Database = sqlx::Sqlite>,
+    name: &str,
+) -> Result<Directory> {
+    let mut connection = connection.acquire().await?;
+    sqlx::query_as!(Directory, "SELECT * FROM directories WHERE name = ?", name)
+        .fetch_one(&mut *connection)
+        .await
+        .map_err(Into::into)
+}
