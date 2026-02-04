4a552ba2fead3dfc63b73317fe0755f92f631dc3
Blaine <blainenicholls@protonmail.com>
2026-02-04 15:41:08 -0500

    feat(api): add more endpoints for job manager

--- src/api/jobs.rs
+++ src/api/jobs.rs
@@ -9,7 +9,7 @@ use axum::{
 use serde::Serialize;
 use ts_rs::TS;
 
-use crate::state::{AppState, Job, JobExecutionReport, JobStateId};
+use crate::state::{AppState, Job, JobExecutionReport, JobId, JobManager, JobState, JobStateId};
 
 #[derive(Debug, Serialize, TS)]
 #[ts(export, export_to = "bindings.ts")]
@@ -23,12 +23,41 @@ pub struct RegistryJob {
 pub fn router() -> Router<AppState> {
     Router::new()
         .route("/api/jobs/{id}/queue", post(queue_job).get(queued_job_id))
+        .route("/api/jobs/{id}/report", get(job_report))
+        .route("/api/jobs/state/{id}", get(state))
         .route("/api/jobs", get(list_jobs))
 }
 
-async fn list_jobs(State(app): State<AppState>) -> Result<Json<Vec<RegistryJob>>> {
-    let jobs = app
-        .job_manager
+async fn queued_job_id(
+    State(app): State<AppState>,
+    Path(id): Path<String>,
+) -> Result<Json<JobStateId>> {
+    Ok(Json(app.job_manager.unique_job_state_id(&id).await?))
+}
+
+async fn queue_job(
+    State(manager): State<Arc<JobManager>>,
+    Path(id): Path<JobId>,
+) -> Result<Json<JobStateId>> {
+    Ok(Json(manager.queue(id, true, true).await?.id()))
+}
+
+async fn job_report(
+    State(manager): State<Arc<JobManager>>,
+    Path(job_id): Path<JobId>,
+) -> Result<Json<JobExecutionReport>> {
+    Ok(Json(manager.job_report(&job_id).await?))
+}
+
+async fn state(
+    State(manager): State<Arc<JobManager>>,
+    Path(state_id): Path<JobStateId>,
+) -> Result<Json<JobState>> {
+    Ok(Json(manager.job_state(state_id).await?))
+}
+
+async fn list_jobs(State(manager): State<Arc<JobManager>>) -> Result<Json<Vec<RegistryJob>>> {
+    let jobs = manager
         .registry()
         .jobs()
         .iter()
@@ -41,18 +70,4 @@ async fn list_jobs(State(app): State<AppState>) -> Result<Json<Vec<RegistryJob>>
         .collect::<Vec<_>>();
 
     Ok(Json(jobs))
-}
-
-async fn queued_job_id(
-    State(app): State<AppState>,
-    Path(id): Path<String>,
-) -> Result<Json<JobStateId>> {
-    Ok(Json(app.job_manager.unique_job_state_id(&id).await?))
-}
-
-async fn queue_job(
-    State(app): State<AppState>,
-    Path(id): Path<String>,
-) -> Result<Json<JobStateId>> {
-    Ok(Json(app.job_manager.queue(id, true, true).await?.id()))
 }
