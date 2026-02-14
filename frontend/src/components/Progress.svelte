a87c32f011e6a46bcb01f4b7fb8f0ccd3f305404
Blaine <blainenicholls@protonmail.com>
2026-02-14 12:30:48 -0500

    style(components): fix progress styling on chorme

--- frontend/src/components/Progress.svelte
+++ frontend/src/components/Progress.svelte
@@ -1,34 +1,90 @@
 <script lang="ts">
 	import type { HTMLProgressAttributes } from "svelte/elements";
 
-	interface Props extends HTMLProgressAttributes {}
+	type Variant = "primary" | "secondary" | "base";
 
-	let { value = $bindable(0), max = 100, ...rest }: Props = $props();
+	interface Props extends HTMLProgressAttributes {
+		variant?: Variant;
+	}
+
+	let {
+		value = $bindable(0),
+		max = 100,
+		variant = "primary",
+		class: className,
+		...rest
+	}: Props = $props();
 </script>
 
 <progress
 	{max}
 	{value}
 	{...rest}
-	class={["h-2 rounded-theme-sm bg-base-400 overflow-hidden", rest.class || ""]}
+	class={[
+		"h-2 rounded-theme-sm bg-base-400 overflow-hidden",
+		className,
+		variant && `text-${variant}`,
+	]}
 >
 	{(Number(value) * 100) / Number(max)}%
 </progress>
 
 <style>
-	progress {
-		-webkit-appearance: none;
-		-moz-appearance: none;
-		appearance: none;
+	@layer components {
+		progress {
+			&:indeterminate {
+				background-image: repeating-linear-gradient(
+					90deg,
+					currentColor -1%,
+					currentColor 10%,
+					#0000 10%,
+					#0000 90%
+				);
+				background-size: 200%;
+				background-position-x: 15%;
+				@media (prefers-reduced-motion: no-preference) {
+					animation: progress 5s ease-in-out infinite;
+				}
 
-		&::-webkit-progress-bar,
-		&::-moz-progress-bar {
-			background-color: var(--color-base-400);
-		}
+				@supports (-moz-appearance: none) {
+					&::-moz-progress-bar {
+						@media (prefers-reduced-motion: no-preference) {
+							animation: progress 5s ease-in-out infinite;
+							background-image: repeating-linear-gradient(
+								90deg,
+								currentColor -1%,
+								currentColor 10%,
+								transparent 10%,
+								transparent 90%
+							);
+							background-size: 200%;
+							background-position-x: 15%;
+						}
+					}
+				}
+			}
 
-		&::-webkit-progress-value,
-		&::-moz-progress-bar {
-			background-color: var(--color-primary);
+			@supports (-moz-appearance: none) {
+				&::-moz-progress-bar {
+					background-color: currentColor;
+				}
+			}
+
+			@supports (-webkit-appearance: none) {
+				&::-webkit-progress-bar {
+					background-color: transparent;
+				}
+
+				&::-webkit-progress-value {
+					background-color: currentColor;
+				}
+			}
+
+			@keyframes progress {
+				50% {
+					background-position-x: -115%;
+				}
+			}
 		}
 	}
 </style>
