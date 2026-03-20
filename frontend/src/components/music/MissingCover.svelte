92b2b43bdd0a394bf1d5dabf93256ed5c3205e36
Blaine <blainenicholls@protonmail.com>
2026-03-20 00:57:17 -0400

    refactor(MissingCover): add HTMLAttributes as props so I can add classes

--- frontend/src/components/music/MissingCover.svelte
+++ frontend/src/components/music/MissingCover.svelte
@@ -1,12 +1,20 @@
-<div class="absolute inset-0 bg-primary-500 bg-blend-multiply"></div>
+<script lang="ts">
+	import type { HTMLAttributes } from "svelte/elements";
+
+	interface Props extends HTMLAttributes<HTMLDivElement> {}
+
+	let { class: className, ...props }: Props = $props();
+</script>
+
+<div class={["bg-primary-500 bg-blend-multiply", className]} {...props}></div>
 
 <style>
 	div {
 		aspect-ratio: 1 / 1;
-		background-image: url("../../assets/missing-album.svg");
 		background-position: center;
 		background-size: 125%;
 		background-color: var(--color-primary-500);
 		background-blend-mode: multiply;
+		background-image: url("../../assets/missing-album.svg");
 	}
 </style>
