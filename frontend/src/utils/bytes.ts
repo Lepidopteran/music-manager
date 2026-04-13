9d7ea78e7e6cfd1612c4882279efe5b4119db9e3
Blaine <blainenicholls@protonmail.com>
2026-04-13 17:44:55 -0400

    refactor(frontend): add full bytes names to byte text formatting util

--- frontend/src/utils/bytes.ts
+++ frontend/src/utils/bytes.ts
@@ -1,22 +1,33 @@
-export function formatBytes(bytes: number | bigint, decimals = 2) {
-	if (typeof bytes !== "number" && typeof bytes !== "bigint") return "0 Bytes";
+export interface FormatBytesOptions {
+	decimals?: number;
+	fullName?: boolean;
+}
+
+interface ByteFormat {
+	short: string;
+	full: string;
+}
+
+export function formatBytes(bytes: number | bigint, options: FormatBytesOptions = {}) {
+	const { decimals = 2, fullName = false } = options;
+	if (typeof bytes !== "number" && typeof bytes !== "bigint") return fullName ? "0 Bytes" : "0 B";
 
 	const k = 1024;
 	const dm = decimals < 0 ? 0 : decimals;
-	const sizes = [
-		"Bytes",
-		"KiB",
-		"MiB",
-		"GiB",
-		"TiB",
-		"PiB",
-		"EiB",
-		"ZiB",
-		"YiB",
+	const sizes: ByteFormat[] = [
+		{ short: "B", full: "Bytes" },
+		{ short: "KiB", full: "Kilobytes" },
+		{ short: "MiB", full: "Megabytes" },
+		{ short: "GiB", full: "Gigabytes" },
+		{ short: "TiB", full: "Terabytes" },
+		{ short: "PiB", full: "Petabytes" },
+		{ short: "EiB", full: "Exabytes" },
+		{ short: "ZiB", full: "Zettabytes" },
+		{ short: "YiB", full: "Yottabytes" },
 	];
 
 	const i = Math.floor(Number(bytes) === 0 ? 0 : Math.log(Number(bytes)) / Math.log(k));
 	const formattedBytes = (Number(bytes) / k ** i).toFixed(dm);
 
-	return `${Number.parseFloat(formattedBytes)} ${sizes[i]}`;
+	return `${Number.parseFloat(formattedBytes)} ${fullName ? sizes[i].full : sizes[i].short}`;
 }
